76f5e7d066fb46420deb2930db0eb6c81b6bbde9
HiZhaoxiaoyang <5859668@qq.com>
2020-09-09 19:49:53 +0800

    frontend: slider init.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,48 @@
 import React from "react";
 import "./App.css";
+import "./App.scss";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+
+import 'swiper/swiper.scss';
+import 'swiper/components/navigation/navigation.scss';
+import 'swiper/components/pagination/pagination.scss';
+import 'swiper/components/scrollbar/scrollbar.scss';
 
 function App() {
-  return <div className="App">{/* write your component here */}</div>;
+  SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
+  return <div className="App">
+      {/* write your component here */}
+      {/* test */}
+      <Swiper
+        spaceBetween={500}
+        slidesPerView={1}
+        // navigation
+        // autoHeight={true}
+        pagination={{ clickable: true }}
+        // scrollbar={{ draggable: true }}
+        onInit={
+          (swiper) => {
+            console.log('swiper initialized');
+          }
+        }
+        onSwiper={(swiper) => {
+          console.log(swiper)
+        }}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide>
+          xPhone
+        </SwiperSlide>
+        <SwiperSlide>
+          Slide 2
+        </SwiperSlide>
+        <SwiperSlide>
+          Slide 3
+        </SwiperSlide>
+      </Swiper>
+    </div>;
 }
 
 export default App;
